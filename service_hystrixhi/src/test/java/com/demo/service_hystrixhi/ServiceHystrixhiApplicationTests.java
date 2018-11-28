fe7408dd9dad172e05ff32b81e8642031466a3b0
ManggoJH <huangweigen2020@163.com>
2018-11-28 17:14:56 +0800

    add Hystrix Turbine
    断路器聚合监控(Hystrix Turbine)(Finchley版本)

---
+++ service_hystrixhi/src/test/java/com/demo/service_hystrixhi/ServiceHystrixhiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.service_hystrixhi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceHystrixhiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ service_hystrixlucy/src/test/java/com/demo/service_hystrixlucy/ServiceHystrixlucyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.service_hystrixlucy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceHystrixlucyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
